5733ff5901ada120139a6ab71a7c8352680dd757
alci19 <bytonalci@gmail.com>
2026-05-17 22:38:31 -0500

    Creamos funciones para crear y listar clientes

--- simulador_completo.js
+++ simulador_completo.js
@@ -29,4 +29,39 @@ function guardarTasa(){
     mostrarTexto("mensajeTasa","Valor de tasa de interés no válido. debe estar entre 10% y 20%");
   }
 }
+
+function guardarCliente(){
+  let cedula = recuperaraTexto("txtCedula");
+  let nombre = recuperaraTexto("txtNombre");
+  let apellido = recuperaraTexto("txtApellido");
+  let ingresos = recuperarFloat("txtIngresos");
+  let egresos = recuperarFloat("txtEgresos");
+  let nuevoCliente = {}
+  
+  nuevoCliente.cedula = cedula;
+  nuevoCliente.nombre = nombre;
+  nuevoCliente.apellido = apellido;
+  nuevoCliente.ingresos = ingresos;
+  nuevoCliente.egresos = egresos;
+  clientes.push(nuevoCliente);
+  pintarClientes();
+}
+
+pintarClientes = function(){ 
+  let cmpTabla = document.getElementById("tablaClientes");
+  let contenidoTabla = "";
+  for(let i=0; i<clientes.length; i++){
+    let cliente = clientes[i];
+    contenidoTabla += "<tr>"+
+    "<td>" + cliente.cedula + "</td>"+ 
+    "<td>" + cliente.nombre + "</td>"+
+    "<td>" + cliente.apellido + "</td>"+
+    "<td>" + cliente.ingresos + "</td>"+
+    "<td>" + cliente.egresos + "</td>"+
+    "<td><button>Actualizar</button></td>"+
+    "</tr>";
+  }
+
+  cmpTabla.innerHTML = contenidoTabla;
+}
 //Para recuperar o mostrar información usar los métodos de la clase utilitarios, puede agregar métodos adicionales en utilitarios
